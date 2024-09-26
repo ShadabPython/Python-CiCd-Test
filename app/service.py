2c3b1b3f201caabf1481af82a3afb8f879c51e6a
Shadab Ahmad <cons-shadab.ahmad@1mg.com>
2024-09-26 09:21:42 +0530

    test115

--- app/service.py
+++ app/service.py
@@ -9,5 +9,5 @@ print("hello world v2")
 
   aws s3 sync . s3://dp-codebase-dev/Python-CiCd-Test --exclude ".git/*" --exclude ".github/*" --size-only
 
-  aws s3 sync . s3://dp-codebase-dev/Python-CiCd-Test --exclude ".git/*" --exclude ".github/*" --exact-timestamps --size-only c
+  aws s3 sync . s3://dp-codebase-dev/Python-CiCd-Test --exclude ".git/*" --exclude ".github/*" --exact-timestamps --size-only cd
 """
